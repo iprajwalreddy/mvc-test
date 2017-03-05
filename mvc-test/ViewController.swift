d0efa75c7226328ca33009271e0e8899c57bc54a
Prajwal Reddy <prajwalreddy@Ps-iMac.local>
2017-03-05 17:09:13 +0530

    Understood about controllers and a few errors

--- mvc-test/ViewController.swift
+++ mvc-test/ViewController.swift
@@ -10,16 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var renameField: UITextField!
+    @IBOutlet weak var fullName: UILabel!
+    let person = Person(first: "John", last: "Hancock")
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        fullName.text = person.fullName
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func btnPressed(_ sender: Any) {
+        
+        if let txt = renameField.text {
+            person.firstName = txt
+            fullName.text = person.fullName
+        }
     }
-
-
 }
 
